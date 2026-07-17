e23fb246352611c800764fe49bea1c41201e4752
srudnytsky <s.rudnytsky@student.neoversity.com.ua>
2026-07-17 17:00:06 +0300

    Create sequelize.js

---
+++ flora-backend/db/sequelize.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        logging: false,
+      }
+    );
+
+module.exports = sequelize;
